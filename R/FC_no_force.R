21181c0f213bd87321faffa43ec5456a2670d7b6
GraceRidder <122539270+GraceRidder@users.noreply.github.com>
2025-02-07 12:58:43 +0100

    Update noforce

--- R/FC_no_force.R
+++ R/FC_no_force.R
@@ -1,20 +1,8 @@
 #SAD generators
-library(sads)
 
-MakeLogNormalSAD = function(matlist, sadmarg, Jmax){
+NoForce(res1$matrix_list, Jmax)
 
-
-  matlist <- res1$matrix_list
-  Jmax <- c(600)
-  sadmarg <- .1
-
-
-  matnames <- unmatrixlist(matlist)
-  matab <- unlist(matlist)
-  reltol <- sadmarg
-  S <- length(matnames)
-  nmatab <- -matab
-
+NoForce = function(matlist, Jmax){
 
   SV <- c()
   for (i in 1:length(matlist)) {
@@ -22,101 +10,31 @@ MakeLogNormalSAD = function(matlist, sadmarg, Jmax){
   }
 
   S <- SV
-  S
 
   J <- c()
   for ( i in 1:length(Jmax)){
     J <- append(J, Jmax[i] * (S[i] / (100 + S[i])))
   }
 
-J # new J
-S # number of species
-sum(matab) #old J
 
-
-if (J > sum(matab) {
-  mat
-}
-J/S
-
-
-
-
-
-
-
-
-
-
-
-Jax <- J
-Sax <- S
-newb <- list()
-
-  for (i in 1:length(Jmax)){
-
-    if (length(matlist[[i]]) > 5){
-
-    J <- Jax[i]
-    S <- Sax[i]
-
-
-
-####  log-normal
-mm <- 0 # mean
-## so I just start with a mean of zero and increase it until I get close..
-#and then repeat the random generation until I'm within a 10% margin of J
-newabund<- rlnorm(S, meanlog = mm, sdlog = 1)
-
-if (sum(newabund) < J){
-  repeat{
-    mm <- mm + .01
-    newabund<- rlnorm(S, meanlog = mm, sdlog = 1)
-    if(sum(newabund) < J+(J*reltol) && sum(newabund) > J-(J*reltol)) break
-  }
-}
-    newb[[i]] <- newabund
-    } else {
-      if ((matlist[[i]][1] != 0)) {
-        newabund <- as.numeric(matlist[[i]][, 1])
-        newb[[i]] <- newabund
-      } else {
-        newb[[i]] <- matlist[[i]]
-      }
-    }
-
-  }
-
-  newabund<- newb
-
-
-  for (i in 1:length(matlist)) {
-    newabund[[i]] <- sort(newabund[[i]], decreasing = T)
-  }
-
-
-#attribute new sizes
-
-### if species have the same abundance they are randomly placed in different ranks (within the original rank zone)
-rankab <- matlist
-speciesrank <- matlist
-
-for (i in 1:length(matlist)){
-  speciesrank[[i]] =rank(-matlist[[i]], ties.method = "random")
-  rankab[[i]]<- newabund[[i]][speciesrank[[i]]]
+oldJ <- c()
+for ( i in 1:length(matlist)){
+  old <- sum(matlist[[i]])
+  oldJ <- append(oldJ, old)
 }
 
+newamt <- matlist
 
-matlist
-mat <- matlist
-for (i in 1:length(matlist)){
-  for (k in 1:length(matlist[[i]])){
-    mat[[i]][k] <- rankab[[i]][k]
+for (i in 1:length(matlist)) {
+  if (oldJ[[i]]  > J[[i]]) {
+    newamt[[i]] <- newamt[[i]] - (oldJ[[i]] - J[[i]]) / S[i]
+  } else {
+    newamt[[i]] <- newamt[[i]] + (oldJ[[i]] + J[[i]]) / S[i]
   }
+
 }
 
-
-return(mat)
+return(newamt)
 
 }
 
